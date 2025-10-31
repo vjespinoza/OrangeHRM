9efabf739aa0b11f0fa2d1512c1f59988641ae33
Victor Espinoza <vjespinoza84@gmail.com>
2025-10-31 14:30:41 -0500

    Optimize VerifyTableConnectionTest test

--- Tests/DataTests/DbConnectionTest.cs
+++ Tests/DataTests/DbConnectionTest.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace OrangeHRM.Tests.DataTests;
 
 public class DbConnectionTest : BaseDataTest
@@ -5,9 +7,17 @@ public class DbConnectionTest : BaseDataTest
     [Fact]
     public void VerifyTableConnectionTest()
     {
-        var query = "SELECT * FROM db_hotel_test.hotel_reservations LIMIT 10";
-        var resul = DbHelper.ExecuteQuery(query);
+        var schemaName = "db_hotel_test";
+        var tableName = "hotel_reservations";
+        var query = $@"SELECT * 
+                   FROM INFORMATION_SCHEMA.TABLES 
+                   WHERE TABLE_SCHEMA = '{schemaName}' 
+                   AND TABLE_NAME = '{tableName}'";
 
-        Assert.NotNull(resul);
+        var result = DbHelper.ExecuteQuery(query);
+
+        Assert.NotNull(result);
+        Assert.True(result.Rows.Count > 0,
+            $"Table '{tableName}' does not exist in schema '{schemaName}'");
     }
 }
